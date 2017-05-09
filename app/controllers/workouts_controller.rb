3925151e4d4fb328f1430f163f636dccf722661b
Jordan Rushing <Jordan@patentmonk.com>
2017-05-09 09:25:46 -0700

    Add Prev & Next buttons

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,6 +1,8 @@
 class WorkoutsController < ApplicationController
   def today
     @workout = Workout.find_by("DATE(scheduled_at) = CURDATE()")
+    @prev = Workout.find_by("DATE(scheduled_at) = SUBDATE(CURDATE(),1)")
+    @next = Workout.find_by("DATE(scheduled_at) = (CURDATE() + INTERVAL 1 DAY)")
 
     if @workout.nil?
       d = Workout.last.distance
@@ -15,10 +17,19 @@ class WorkoutsController < ApplicationController
     @workouts_distance = Workout.all.pluck(:distance).sum
   end
 
+  def show
+    @workout = Workout.find(params[:id])
+    @prev = @workout.prev
+    @next = @workout.next
+
+    @worked_out_distance = Workout.where.not(completed_at: nil).pluck(:distance).sum
+    @workouts_distance = Workout.all.pluck(:distance).sum
+  end
+
   def update
     workout = Workout.find(params[:id])
     workout.update(workout_params)
-    redirect_to root_path
+    redirect_to workout
   end
 
   private
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,4 +3,12 @@ class Workout < ActiveRecord::Base
   def completed?
     !completed_at.nil?
   end
+
+  def next
+    Workout.find_by(scheduled_at: self.scheduled_at + 1.day)
+  end
+
+  def prev
+    Workout.find_by(scheduled_at: self.scheduled_at - 1.day)
+  end
 end
